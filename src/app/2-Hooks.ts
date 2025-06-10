64faabef7f5ed63fc1723f60791aa9c501c5392b
mpsalunggg <putrasatria893@gmail.com>
2025-06-10 22:23:56 +0700

    testing error

--- src/app/2-Hooks.ts
+++ src/app/2-Hooks.ts
@@ -2,6 +2,9 @@ import { toUpperCase } from './Utils'
 
 export class StringUtils {
   public toUpperCase(arg: string) {
+    if(!arg){
+      throw new Error("Invalid arguments!")
+    }
     return toUpperCase(arg)
   }
 }
--- src/test/2-Hooks.test.ts
+++ src/test/2-Hooks.test.ts
@@ -3,13 +3,13 @@ import { StringUtils } from '../app/2-Hooks'
 describe('Hooks jest test suite', () => {
   let sut: StringUtils
 
-  beforeAll(() => {
-    console.log('>> beforeAll: Starting test suite')
-  })
+  // beforeAll(() => {
+  //   console.log('>> beforeAll: Starting test suite')
+  // })
 
-  afterAll(() => {
-    console.log('>> afterAll: Finishing test suite')
-  })
+  // afterAll(() => {
+  //   console.log('>> afterAll: Finishing test suite')
+  // })
 
   beforeEach(() => {
     sut = new StringUtils()
@@ -25,5 +25,32 @@ describe('Hooks jest test suite', () => {
 
     expect(actual).toBe('TEST')
     console.log('Doing Test')
+  })
+
+  it('Should throw error - Function', () => {
+    function expectError() {
+      sut.toUpperCase('')
+    }
+
+    expect(expectError).toThrow('Invalid arguments!')
+    // deprecated
+    // expect(expectError).toThrowError()
+  })
+
+  it('Should throw error - Arrow', () => {
+    expect(() => {
+      sut.toUpperCase('')
+    }).toThrow('Invalid arguments!')
+  })
+
+  it('Should throw error - Try Catch', (done) => {
+    try {
+      sut.toUpperCase('')
+      done('GetStringInfo should throw error for invalid arg!')
+    } catch (error) {
+      expect(error).toBeInstanceOf(Error)
+      expect(error).toHaveProperty('message', 'Invalid arguments!')
+      done()
+    }
   })
 })
